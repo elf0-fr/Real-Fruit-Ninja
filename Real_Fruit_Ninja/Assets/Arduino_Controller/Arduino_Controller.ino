85313091124b80142759ac879647bd62f152e297
Mathis Boiteau <mathis.boiteau@ensiie.fr>
2021-03-29 16:01:36 +0200

    Detection de touché avec message dans le port serial, version oignons

--- Real_Fruit_Ninja/Assets/Arduino_Controller/Arduino_Controller.ino
+++ Real_Fruit_Ninja/Assets/Arduino_Controller/Arduino_Controller.ino
@@ -1,3 +1,19 @@
+//Libraries
+#include <CapacitiveSensor.h>//https://github.com/PaulStoffregen/CapacitiveSensor
+
+//Parameters
+bool autocal  = 0;
+const int numReadings  = 10;
+long readings [numReadings];
+int readIndex  = 0;
+long total  = 0;
+const int sensitivity  = 1000;
+const int thresh  = 200;
+const int csStep  = 10000;
+CapacitiveSensor cs  = CapacitiveSensor(2, 3);
+
+
+
 // The flag signals to the rest of the program an interrupt occured
 static bool button_flag = false;
 // Remember the state the river in the Unity program is in
@@ -8,44 +24,45 @@ void buttonPress() {
   button_flag = true;
 }
 
-void setup() {
-  int buttonPin = 2;
-  
-  pinMode(LED_BUILTIN, OUTPUT);
-  // Internal pullup, no external resistor necessary
-  pinMode(buttonPin,INPUT_PULLUP);
-  // 115200 is a common baudrate : fast without being overwhelming
-  Serial.begin(115200);
 
-  // As the button is in pullup, detect a connection to ground
-  attachInterrupt(digitalPinToInterrupt(buttonPin),buttonPress,FALLING);
+void setup() {
+  //Init Serial USB
+  Serial.begin(115200);
+  //Init cs
+  if (autocal == 0) {
+    {
+      cs.set_CS_AutocaL_Millis(0xFFFFFFFF);
+    }
+  }
 }
 
-// Processes button input
 void loop() {
-  // Slows reaction down a bit
-  // but prevents _most_ button press misdetections
-  delay(200);
-  
-  if (button_flag) {
-    if (river_state) {
-      Serial.println("dry");
-    } else {
-      Serial.println("wet");
-    }
-    river_state = !river_state;
+  int value = smooth();
+  if (value > 1000 && button_flag == false){
+    button_flag = true;
+    Serial.println("Oignon");
+  }
+  if (value < 1000 && button_flag == true) {
     button_flag = false;
   }
 }
 
-// Handles incoming messages
-// Called by Arduino if any serial data has been received
-void serialEvent()
-{
-  String message = Serial.readStringUntil('\n');
-  if (message == "LED ON") {
-    digitalWrite(13,HIGH);
-  } else if (message == "LED OFF") {
-    digitalWrite(13,LOW);
+long smooth() { /* function smooth */
+  ////Perform average on sensor readings
+  long average;
+  // subtract the last reading:
+  total = total - readings[readIndex];
+  // read the sensor:
+  readings[readIndex] = cs.capacitiveSensor(sensitivity);
+  // add value to total:
+  total = total + readings[readIndex];
+  // handle index
+  readIndex = readIndex + 1;
+  if (readIndex >= numReadings) {
+    readIndex = 0;
   }
+  // calculate the average:
+  average = total / numReadings;
+
+  return average;
 }
