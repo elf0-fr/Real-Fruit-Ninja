331edd92708d87a5c731e2bf39bae6bd6d524f04
Mathias DURAND <mathias.durand@ensiie.fr>
2021-03-29 17:37:49 +0200

    trail

---
+++ Real_Fruit_Ninja/Assets/CuttingAnimation.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuttingAnimation : MonoBehaviour
+{
+    [SerializeField] private float speed = 10.0f;
+    private Vector3 tagetPos;
+    [SerializeField] private TrailRenderer trail;
+    [SerializeField] private TrailRenderer childTrail;
+
+    public void Start() 
+    {   
+        trail.enabled = false;
+        childTrail.enabled = false;
+        Random.InitState(System.DateTime.Now.Millisecond);
+        Vector3 randomDirection = new Vector3(0.0f, 0.0f, Random.value);
+        transform.Rotate(randomDirection);
+        transform.Translate(transform.right * 2);
+
+        tagetPos = transform.position + transform.right * 4;
+        trail.enabled = true;
+        childTrail.enabled = true;
+    }
+
+
+    public void Update()
+    {
+        float step =  speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position,tagetPos , step);
+
+    }
+}
